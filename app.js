34fb91488c3e0adde3f0921e31f089a2a20b5ab4
sangdo <sang.do@knorex.com>
2024-12-21 12:13:18 +0700

    - replace routes to api path

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var movieRouter = require('./routes/movie');
+var indexRouter = require('./api/index');
+var usersRouter = require('./api/users');
+var movieRouter = require('./api/movie');
 
 var app = express();
 
@@ -15,7 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
